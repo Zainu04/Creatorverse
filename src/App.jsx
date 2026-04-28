845569649e3bc96bd1a673b84ee300186b2d8ef0
Zainu04 <akhtarzainab2010@gmail.com>
2026-04-28 10:49:19 -0400

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useRoutes } from "react-router-dom";
 
-import ShowCreators from "./pages/ShowCreators.jsx/index.js";
-import ViewCreator from "./pages/ViewCreator.jsx/index.js";
-import AddCreator from "./pages/AddCreator.jsx/index.js";
+import ShowCreators from "./pages/ShowCreators.jsx";
+import ViewCreator from "./pages/ViewCreator.jsx";
+import AddCreator from "./pages/AddCreator.jsx";
 import EditCreator from "./pages/EditCreator.jsx";
 
 export default function App() {
